6b3d91992facd489faf511d56144890bb0417183
ChaseSmith67 <smitcha6@oregonstate.edu>
2023-09-12 21:53:20 -0500

    add player to world

--- lib/p_game.dart
+++ lib/p_game.dart
@@ -1,8 +1,11 @@
 import 'package:flame/game.dart';
 import 'package:flame/components.dart';
+import 'package:p_game/actors/hoot.dart';
 
 class PGame extends FlameGame {
   PGame();
+
+  late HootPlayer _hoot;
 
   final world = World();
   late final CameraComponent cameraComponent;
@@ -18,5 +21,10 @@ class PGame extends FlameGame {
   cameraComponent = CameraComponent(world: world);
   cameraComponent.viewfinder.anchor = Anchor.topLeft;
   addAll([cameraComponent, world]);
+
+  _hoot = HootPlayer(
+      position: Vector2(128, canvasSize.y - 70),
+    );
+    world.add(_hoot);
   }
 }
